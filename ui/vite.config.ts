9cf1af907339ae3077790ae249e9c48208c6cc2e
curefatih <curefat@gmail.com>
2023-06-25 01:38:07 +0300

    update ui asset url

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "8935",
+  base: "./",
   build: {
     chunkSizeWarningLimit: 4000,
     emptyOutDir: true,
