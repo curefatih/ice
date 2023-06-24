501135ae640e1803e4518d626c4dc3108fef6625
curefatih <curefat@gmail.com>
2023-06-25 00:45:20 +0300

    take 2: update base asset urls

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "8935",
   build: {
     chunkSizeWarningLimit: 4000,
     emptyOutDir: true,
